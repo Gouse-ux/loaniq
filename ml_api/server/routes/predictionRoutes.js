ddb867bbf5faa3cd34ce4ca8225e95d367854475
Gouse-ux <234g1a3339@srit.ac.in>
2026-03-23 12:04:21 +0530

    clean up ml_api folder

--- ml_api/server/routes/predictionRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { makePrediction, getHistory } = require('../controllers/predictionController');
-const { protect, admin } = require('../middleware/authMiddleware');
-
-router.post('/', protect, makePrediction);
-router.get('/history', protect, admin, getHistory);
-
-module.exports = router;
